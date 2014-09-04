b4164c2c9e769fd88d5f1ca62211ffbc29104011
Logan Zhou <xorcererzc@gmail.com>
2014-09-04 16:57:28 +0800

    Added Zhangfu's requirement.

---
+++ md5hash.py
@@ -0,0 +1,52 @@
+#!/bin/env python
+
+import os
+import sys
+from hashlib import md5
+
+from dbs.leveldb import DB as BackendDB
+
+
+def log(msg):
+    print msg
+
+
+def extract_json(msg):
+    log('msg: %s' % msg)
+    md5_value, json_str = msg.split(' ', 1)
+    return md5_value, json.loads(json_str)
+
+
+class DB(object):
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+        self.db = BackendDB(db_name, create_if_missing=True)
+
+    def lookup(self, md5):
+        original = self.db.get(md5.decode('hex'))
+        return original
+
+    def query(self, md5_value, msg):
+        original = self.lookup(md5_value)
+        return original
+
+    def put(self, original):
+        self.db.put(md5(original).hexdigest().decode('hex'), original)
+
+    def close(self):
+        self.db.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: %s <password-file>' % sys.argv[0]
+        sys.exit(1)
+
+    db = DB('md5')
+    with open(sys.argv[1]) as f:
+        for l in f:
+            db.put(l.strip())
+            
+    db.close()
